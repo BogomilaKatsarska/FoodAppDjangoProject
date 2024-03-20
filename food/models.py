75a69792b7da3facb6e860458aa51f8b7af6459b
Bogomila <bkatsarska@abv.bg>
2024-03-20 15:14:09 +0200

    added templates, staticfiles, updated Items model, together with URLs

--- food/models.py
+++ food/models.py
@@ -9,6 +9,10 @@ class Item(models.Model):
         max_length=200,
     )
     item_price = models.IntegerField()
-
+    item_image = models.URLField(
+        default='https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg',
+        null=True,
+        blank=True,
+    )
     def __str__(self):
         return self.item_name
--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from food.views import index, item
+from food.views import index, item, detail
+
+app_name = 'food'
 
 urlpatterns = (
     path('', index, name='index'),
+    path('<int:item_id>/', detail, name='detail'),
     path('item/', item, name='item'),
 )
