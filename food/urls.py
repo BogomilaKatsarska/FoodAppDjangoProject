573a69509471d43a924f9316e1a5b5737ad8818c
Bogomila <bkatsarska@abv.bg>
2024-03-25 16:21:33 +0200

    added CBV

--- food/urls.py
+++ food/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from food.views import index, detail, create_item, update_item, delete_item
+from food.views import create_item, update_item, delete_item, IndexClassView, FoodDetail
 
 app_name = 'food'
 
 urlpatterns = (
-    path('', index, name='index'),
-    path('<int:item_id>/', detail, name='detail'),
+    # path('', index, name='index'),
+    path('', IndexClassView.as_view(), name='index'),
+    # path('<int:item_id>/', detail, name='detail'),
+    path('<int:pk>/', FoodDetail.as_view(), name='detail'),
     path('add/', create_item, name='create_item'),
     path('update/<int:id>/', update_item, name='update_item'),
     path('delete/<int:id>/', delete_item, name='delete_item'),
